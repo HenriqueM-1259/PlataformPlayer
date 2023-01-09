7e42056442c766feb941ae59331a440a4e2c043b
Marcelo Henrique <Mhsf15@hotmail.com>
2023-01-08 23:55:32 -0300

    Configuracoes completas

--- src/configs/elementConfig.js
+++ src/configs/elementConfig.js
@@ -2,7 +2,7 @@ class elementConfig extends canvasConfig{
     constructor(pos,length){
         super()
         this.pos = {
-            X:pos.X,Y:pos.Y
+            X:pos.X ,Y:pos.Y
         }
         this.length = {
             X:length.X,Y:length.Y
